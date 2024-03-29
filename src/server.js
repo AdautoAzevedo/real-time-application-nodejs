3f159727f6beac7dbadabf26114e3dde6b76d074
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-03-29 16:09:11 -0300

    Added a way to differentiate users

--- src/server.js
+++ src/server.js
@@ -7,18 +7,35 @@ const path = require('path');
 const PORT = process.env.PORT || 3500;
 const server = http.createServer(app);
 const io = socketIo(server);
+const connectedUser = new Map();
 
 io.on('connection', (socket) => {
     console.log("User connected: ", socket.id);
-    console.log("User connected: ");
+
+    socket.on('login', (username) => {
+        connectedUser.set(socket.id, username);
+        io.emit('message', {
+            username: 'Server', 
+            message: `${username} joined the chat`
+        })
+    })
 
     socket.on('message', (data) => {
-        console.log(data);
-        io.emit('message', data);
+        const username = connectedUser.get(socket.id);
+        io.emit('message',{
+            username,
+            message: data
+        });
     });
 
     socket.on('disconnect', () => {
         console.log("User disconnected");
+        const username = connectedUser.get(socket.id);
+        connectedUser.delete(socket.id);
+        io.emit('message', {
+            username: 'Server',
+            message: `${username} left the chat`
+        })
     })
 });
 
