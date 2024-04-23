cb859e73a33ed260097fa84f07bbea608f728b95
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-04-23 20:51:41 -0300

    Added socket functions

--- client/chat-app/src/App.js
+++ client/chat-app/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import io from 'socket.io-client';
 
 function App() {
+  const serverURL = "http://localhost:3500";
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [userList, setUserList] = useState([]);
+  const [selectedUser, setSelectedUser] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io(serverURL);
+    setSocket(newSocket);
+  
+    return () => {
+      newSocket.disconnect();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on('receiveMessage', (data) => {
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+
+    socket.on('userList', (data) => {
+      setUserList(data);
+    })
+  
+    return () => {
+      socket.off('receiveMessage');
+      socket.off('userList');
+    }
+  }, [socket]);
+
+  const handleSendMessage = () => {
+    if (!selectedUser || !message) return;
+    socket.emit('sendMessage', {to: selectedUser, message});
+    setMessage('');
+  };
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
